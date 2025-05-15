ac40eb202a9fbeb3961b2f715deb27d2765c3db5
Hernán Sánchez <nachos1sr@gmail.com>
2025-05-15 06:32:31 -0500

    Ajustes código

--- src/main.py
+++ src/main.py
@@ -1,14 +1,3 @@
-'''
-Reference implementation of node2vec. 
-
-Author: Aditya Grover
-
-For more details, refer to the paper:
-node2vec: Scalable Feature Learning for Networks
-Aditya Grover and Jure Leskovec 
-Knowledge Discovery and Data Mining (KDD), 2016
-'''
-
 import argparse
 import numpy as np
 import networkx as nx
@@ -16,9 +5,6 @@ import node2vec
 from gensim.models import Word2Vec
 
 def parse_args():
-    '''
-    Parses the node2vec arguments.
-    '''
     parser = argparse.ArgumentParser(description="Run node2vec.")
 
     parser.add_argument('--input', nargs='?', default='graph/karate.edgelist',
@@ -64,9 +50,6 @@ def parse_args():
     return parser.parse_args()
 
 def read_graph():
-    '''
-    Reads the input network in networkx.
-    '''
     if args.weighted:
         G = nx.read_edgelist(args.input, nodetype=int, data=(('weight',float),), create_using=nx.DiGraph())
     else:
@@ -80,20 +63,13 @@ def read_graph():
     return G
 
 def learn_embeddings(walks):
-    '''
-    Learn embeddings by optimizing the Skipgram objective using SGD.
-    '''
     walks = [list(map(str, walk)) for walk in walks]
     model = Word2Vec(walks, vector_size=args.dimensions, window=args.window_size,
                      min_count=0, sg=1, workers=args.workers, epochs=args.iter)
-    model.save_word2vec_format(args.output)
-
+    model.wv.save_word2vec_format(args.output)
     return
 
 def main(args):
-    '''
-    Pipeline for representational learning for all nodes in a graph.
-    '''
     nx_G = read_graph()
     G = node2vec.Graph(nx_G, args.directed, args.p, args.q)
     G.preprocess_transition_probs()
